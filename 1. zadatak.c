2a86d714f0fe92c13e1314f006f3f2dd6b5fbfda
dacok5 <91673172+dacok5@users.noreply.github.com>
2021-09-30 17:52:39 +0200

    Add files via upload

---
+++ 1. zadatak.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void) {
+	int a;
+	int b;
+	int c;
+	scanf("%d%d", &a, &b);
+	if (a > b) {
+		c = a;
+	}
+	else {
+		c = b;
+	}
+	printf("%d", c);
+	return 0;
+}
+
+/* u ovom vrlo jednostavnom kodu dajemo korisniku da unese dva broja te s if() petljom provjeravamo uvjete, u ovom
+slučaju prvojeravamo je li a>b, ako je uvjet istinit tj. da je a veći od b, a spremamo u varijablu c, ako nije uvjet
+istinit else() petljom spremamo vrijednost od b u novu varijablu c.* 
+ 
